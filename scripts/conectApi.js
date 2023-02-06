971da51fc37f6c6f1fcf3caa07d10acbbfd07be2
Lucas Costa <103662624+Lucascmo8@users.noreply.github.com>
2023-02-06 20:31:55 -0300

    Criado o HTML e conectando a API

---
+++ scripts/conectApi.js
@@ -0,0 +1,10 @@
+const urlApi = `https://the-trivia-api.com/api/questions?categories=geography,film_and_tv,food_and_drink,general_knowledge,history,music,science,society_and_culture,sport_and_leisure,arts_and_literature&limit=15&region=BR&difficulty=medium`
+let quiz = []
+
+conectApi(urlApi)
+async function conectApi(url){
+    const res = await fetch(url)
+    quiz = await res.json()
+    console.log(quiz)
+}
+
